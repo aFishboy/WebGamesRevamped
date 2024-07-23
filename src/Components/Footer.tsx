b07d8b306078db3017f3749f53f39a535562e879
Samuel Howard <srhoward@ucdavis.edu>
2024-07-22 23:38:53 -0700

    responsive game cards

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
+import copyRightSign from "../assets/images/copyRightSign.svg";
 
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
-  return (
-    <footer className='bg-black text-white'>
-      <div className='container mx-auto py-4 flex flex-col md:flex-row justify-between items-center'>
-        <div className='flex flex-col md:flex-row justify-between items-center'>
-          <div className='text-sm'>
-            <p>© 2023 Games</p>
-          </div>
-        </div>
-      </div>  
-    </footer>
-  )
-}
+    return (
+        <footer className="max-container">
+            <div className="flex justify-between bg-black h-[200px] mt-24 max-sm:flex-col max-sm:items-center">
+                <div className="flex flex-1 justify-center items-center text-white gap-2 font-montserrat cursor-pointer">
+                    <img
+                        src={copyRightSign}
+                        alt="copyright sign"
+                        width={20}
+                        height={20}
+                        className="rounded-full m-0"
+                    />
+                    <p>Copyright. All rights reserved.</p>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
